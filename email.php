66549aa2d3a2cb844514c5aead41963a62963979
HamzaBilal2003 <hamza@hmstech.org>
2025-05-17 13:18:31 +0500

    done

---
+++ email.php
@@ -0,0 +1,31 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host = 'ironfoot.onlinehosting.co.za';
+    $mail->Port = 25;
+    $mail->SMTPAuth = false; // No auth
+    $mail->SMTPSecure = false; // No encryption
+
+    // Sender and recipient
+    $mail->setFrom('support@sautech.net', 'Their Name');
+    $mail->addAddress('xamzabilal2003@gmail.com');
+
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Email';
+    $mail->Body    = 'This is a test email.';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Mailer Error: {$mail->ErrorInfo}";
+}
+?>
