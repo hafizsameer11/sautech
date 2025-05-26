456b9ba32820b48cac24b2f7ee8b510882648b8d
unknown <hmstech11@gmail.com>
2025-05-26 19:52:30 +0500

    helo

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ ini_set('display_errors', 1);
 
 session_start();
 include('./modules/components/permissioncheck.php');
-if (!isset($_SESSION['user_id'])) {
-  header("Location: modules/auth/login.php");
-  exit();
-}
+// if (!isset($_SESSION['user_id'])) {
+//   header("Location: modules/auth/login.php");
+//   exit();
+// }
 if (isset($_GET['logout']) && $_GET['logout'] == 1) {
   session_destroy();
   header("Location: modules/auth/login.php");
