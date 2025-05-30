abce4f4dc3ae4f1fb489c8f78f40c558f2062e75
HamzaBilal2003 <hamza@hmstech.org>
2025-05-30 15:16:05 +0500

    expense

--- modules/billing/expenses.php
+++ modules/billing/expenses.php
@@ -1,5 +1,8 @@
 <?php
 // DB connection
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include_once '../config.php'; // Ensure this path is correct
 
 // Add Expense
